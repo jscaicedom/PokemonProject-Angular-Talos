2495abe3fc358c883b305555a143dee041239c00
Johana Sarai Caicedo <johanasaraicaicedo@Ricardos-MacBook-Pro.local>
2020-11-10 09:22:29 -0500

    main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,8 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import '../node_modules/chart.js/dist/Chart.js';
-import '../node_modules/hammerjs/hammer.min.js';
+import 'chart.js';
+import 'hammerjs';
 
 if (environment.production) {
   enableProdMode();
