d796254dacc0f836c311e1005808401f0896ec58
System Administrator <root@Ricardos-MacBook-Pro.local>
2020-11-11 09:23:51 -0500

    Auto-generated commit

--- src/app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import { PokemonsHttpService } from 'src/app/services/pokemons-http.service';
-
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-})
-export class AppComponent {}
--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  apiUrl: 'https://pokeapi.co/api/v2/pokemon/?limit=20&offset=',
-  selectedUrl: 'https://pokeapi.co/api/v2/pokemon/',
-  imageUrl:
-    'https://raw.githubusercontent.com/PokeAPI/sprites/146c91287ad01f6e15315bbd733fd7442c91fe6d/sprites/pokemon/',
-  production: true,
-};
